abe4cc08fe8861ede91970eb22e09b73aa71291d
jzlshitian <243777820@qq.com>
2016-04-02 15:27:32 +0800

    ex1_multi

--- machine-learning-ex1/ex1/computeCostMulti.m
+++ machine-learning-ex1/ex1/computeCostMulti.m
@@ -13,7 +13,8 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
+delta = X*theta - y;
+J = (1/(2.*m))*delta'*delta;
 
 
 
--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -104,8 +104,9 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+size_norm = (1650 - mu(1))/sigma(1);
+br_norm = (3 - mu(2))/sigma(2);
+price = [1, size_norm, br_norm]*theta;
 
 % ============================================================
 
@@ -149,7 +150,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3]*theta;
 
 
 % ============================================================
